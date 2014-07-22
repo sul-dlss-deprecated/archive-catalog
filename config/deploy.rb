ddcab1bd9ced144c3ca377abb4fbf161ef5ed45d
Richard Anderson <rnanders@stanford.edu>
2014-07-22 04:59:57 -0600

    add tmp folder link to shared/tmp

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w(log config/environments)
+set :linked_dirs, %w(log tmp config/environments)
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
