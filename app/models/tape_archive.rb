261542714143fe558f6fa62f0daefe5f65af3169
Richard Anderson <rnanders@stanford.edu>
2014-07-31 10:52:14 -0600

    Revise model primary keys and constraints,  version bump

--- app/models/tape_archive.rb
+++ app/models/tape_archive.rb
@@ -1,5 +1,6 @@
 class TapeArchive  < ActiveRecord::Base
   self.primary_key = :tape_archive_id
+  validates :tape_archive_id, :presence => true
 
   # @return [String] the %{model} instance identifier string used in flashes or other captions.  See config/locales files
   def to_s
