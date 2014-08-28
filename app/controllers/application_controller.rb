cfd520ff5c79dc3e66645c27aeca020c98bcecf0
Darren Weber <dlweber@stanford.edu>
2014-08-28 15:52:05 -0700

    Add notes about authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  # TODO: Add authentication
+  # TODO: Update sdr-pc config with authentication config, see
+  # TODO: sdr-preservation-core/config/environments/integration.rb
+
 end
